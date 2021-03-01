8c71883869123edee169d87fdbe8af683b43136b
heinimaria <hmaria.partanen@gmail.com>
2021-03-01 18:55:30 +0000

    Scraper updated due to website change

--- horoscopes/scraper.py
+++ horoscopes/scraper.py
@@ -9,25 +9,25 @@ url_pisces = requests.get('https://www.astrology.com/horoscope/daily/pisces.html
 
 def get_capricorn():
     soup_capricorn = BeautifulSoup(url_capricorn.text, 'html.parser')
-    daily_capricorn = soup_capricorn.find('p').get_text()
-    return daily_capricorn
+    daily_capricorn = soup_capricorn.find('div', {'id': 'content'}).get_text()
+    return daily_capricorns
 
 
 def get_cancer():
     soup_cancer = BeautifulSoup(url_cancer.text, 'html.parser')
-    daily_cancer = soup_cancer.find('p').get_text()
+    daily_cancer = soup_cancer.find('div', {'id': 'content'}).get_text()
     return daily_cancer
 
 
 def get_gemini():
     soup_gemini = BeautifulSoup(url_gemini.text, 'html.parser')
-    daily_gemini = soup_gemini.find('p').get_text()
+    daily_gemini = soup_gemini.find('div', {'id': 'content'}).get_text()
     return daily_gemini
 
 
 def get_pisces():
     soup_pisces = BeautifulSoup(url_pisces.text, 'html.parser')
-    daily_pisces = soup_pisces.find('p').get_text()
+    daily_pisces = soup_pisces.find('div', {'id': 'content'}).get_text()
     return daily_pisces
 
 
